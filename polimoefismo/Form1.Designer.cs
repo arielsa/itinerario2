53a78e3723ab034dbcdea90916d9117017f48d17
ariel <santa.cruz.unla@gmail.com>
2023-04-20 09:07:10 -0300

    mi cal terminado

--- polimoefismo/Form1.Designer.cs
+++ polimoefismo/Form1.Designer.cs
@@ -28,21 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(89, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "producto";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(89, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 64);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "suma";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(89, 157);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(153, 61);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "resta";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(265, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(137, 53);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(265, 247);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(137, 53);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "0";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(265, 165);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(137, 53);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "0";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- polimoefismo/Form1.cs
+++ polimoefismo/Form1.cs
@@ -21,10 +21,53 @@ namespace polimoefismo
         {
 
         }
+
+        private void EjecutaPolimorfismo(Operacion pOperacion) 
+        {
+            decimal valor = pOperacion.Ejecutar(Convert.ToDecimal(textBox1.Text),Convert.ToDecimal(textBox3.Text));
+
+            textBox2.Text = valor.ToString();            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            EjecutaPolimorfismo(new Suma());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            EjecutaPolimorfismo(new Resta());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EjecutaPolimorfismo(new Producto());
+        }
     }
 
     public abstract class Operacion 
     {
-        
+        public abstract decimal Ejecutar (decimal n1, decimal n2);
+    }
+    public class Suma : Operacion
+    {
+        public override decimal Ejecutar(decimal n1, decimal n2)
+        {
+            return n1 + n2;
+        }
+    }
+    public class Resta : Operacion
+    {
+        public override decimal Ejecutar(decimal n1, decimal n2)
+        {
+            return n1 - n2;
+        }
+    }
+    public class Producto : Operacion
+    {
+        public override decimal Ejecutar(decimal n1, decimal n2)
+        {
+            return n1 * n2;
+        }
     }
 }
